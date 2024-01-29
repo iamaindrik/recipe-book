6f3bc1f5c48b069238bbb5ed8348ac6d95aa3c2e
Aindrik Sarkar <119431351+iamaindrik@users.noreply.github.com>
2024-01-29 19:00:44 +0530

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,42 @@
+
+function tikka() {
+  window.location.href="tikka.html", "_blank";
+}
+  function butter() {
+    window.open("https://www.youtube.com/watch?v=a30BLUQiFoc");
+  }
+  
+  function chili() {
+    window.open("https://www.youtube.com/watch?v=P7ZGxWKJkX8");
+  }
+  
+  function redsauce() {
+    window.open("https://www.youtube.com/watch?v=K1P_KI1YD4c");
+  }
+  
+  function whitesauce() {
+    window.open("https://www.youtube.com/watch?v=l_HOohJZL0U");
+  }
+  
+  function masala() {
+    window.open("https://www.youtube.com/watch?v=UjDfZDsKz2M");
+  }
+  
+  function tawap() {
+    window.open("https://youtu.be/zAICu_dIlUs?feature=shared");
+  }
+  
+  function pulao() {
+    window.open("https://youtu.be/c0BeFR-wMi4?feature=shared");
+  }
+  
+  function anda() {
+    window.open("https://youtu.be/vPkm5GnTE_8?feature=shared");
+  }
+
+  function next() {
+    window.location.href="errorpage.html", "_top";
+  }
+
+
+
